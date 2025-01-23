8f0d33656586c3caa5968f0a336de863f3c47d7d
Michael Michailidis <michael@noesis.solutions>
2025-01-23 10:14:42 +0200

    Added exmple file

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,22 +1,75 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import {
+  BaseEntity,
+  Cascade, Collection,
+  Entity,
+  Enum,
+  ManyToOne,
+  MikroORM,
+  OneToMany,
+  PrimaryKey,
+  Property,
+  Unique
+} from '@mikro-orm/sqlite';
 
-@Entity()
-class User {
+export enum SchoolGrade {
+  GradeOne = '1',
+  GradeTwo = '2',
+  GradeThree = '3',
+  GradeFour = '4',
+  GradeFive = '5',
+  GradeSix = '6',
+}
 
-  @PrimaryKey()
+export enum SubTestType {
+  AdjustVolume = 'adjustVolume',
+  CompareImages = 'compareImages',
+  CompareNumbers = 'compareNumbers',
+}
+
+@Entity({ tableName: 'subtests' })
+export class SubTestEntity {
+  @PrimaryKey({ type: 'number', autoincrement: true })
   id!: number;
 
   @Property()
-  name: string;
+  title!: string;
 
-  @Property({ unique: true })
-  email: string;
+  @Enum(() => SubTestType)
+  type!: SubTestType;
 
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
+  @Enum({ items: () => SchoolGrade, array: true })
+  grades!: SchoolGrade[];
+
+  @OneToMany(
+      () => ExerciseEntity,
+      (exercise: ExerciseEntity) => exercise.subtest,
+      {
+        cascade: [Cascade.ALL],
+        orderBy: { order: 'asc' },
+      },
+  )
+  exercises = new Collection<ExerciseEntity>(this);
+}
+
+@Entity({ tableName: 'exercises' })
+@Unique({ properties: ['order', 'subtest'] })
+export class ExerciseEntity extends BaseEntity {
+  @PrimaryKey({ type: 'number', autoincrement: true })
+  id!: number;
+
+  @Property()
+  order!: number;
+
+  @Enum({ items: () => SchoolGrade, array: true })
+  grades!: SchoolGrade[];
+
+  @ManyToOne(() => SubTestEntity)
+  subtest!: SubTestEntity;
+
+  @Property({ persist: true })
+  get type(): SubTestType {
+    return this.subtest.type;
   }
-
 }
 
 let orm: MikroORM;
@@ -24,7 +77,7 @@ let orm: MikroORM;
 beforeAll(async () => {
   orm = await MikroORM.init({
     dbName: ':memory:',
-    entities: [User],
+    entities: [SubTestEntity, ExerciseEntity],
     debug: ['query', 'query-params'],
     allowGlobalContext: true, // only for testing
   });
@@ -36,16 +89,36 @@ afterAll(async () => {
 });
 
 test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+  /* Create a Subtest for testing */
+  const subtest = new SubTestEntity();
+  subtest.title = 'Subtest One';
+  subtest.type = SubTestType.CompareImages;
+  subtest.grades = [SchoolGrade.GradeFour, SchoolGrade.GradeFive];
+  for (let i: number = 0; i < 5; i++) {
+    const exercise = new ExerciseEntity();
+    exercise.subtest = subtest;
+    exercise.order = i + 1;
+    exercise.grades = [SchoolGrade.GradeFour, SchoolGrade.GradeFive];
+    subtest.exercises.add(exercise);
+    orm.em.persist(exercise);
+  }
   await orm.em.flush();
-  orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  const searchGrade: SchoolGrade = SchoolGrade.GradeFour;
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  const results = await orm.em.find(SubTestEntity, {
+        grades: { $contains: [searchGrade] },
+      },
+      {
+        populate: ['exercises'],
+        populateWhere: {
+          exercises: {
+            grades: {
+              $contains: [searchGrade],
+            },
+          },
+        },
+      });
+
+  expect(results.length).toBe(1);
 });
